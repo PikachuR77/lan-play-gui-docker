d16658c2439c83429ea4c03a99eb9c4e23aa1553
PikachuR77 <36930070+PikachuR77@users.noreply.github.com>
2022-09-11 22:26:07 -0400

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-echo Running with server $(< /config/server.txt)
+echo Running on port $(< /config/port.txt)
 cd /app
 #./lan-play-linux --relay-server-addr $(< /config/server.txt)
-./lan-play-gui
+./lan-play-gui --port $(< /config/port.txt)
